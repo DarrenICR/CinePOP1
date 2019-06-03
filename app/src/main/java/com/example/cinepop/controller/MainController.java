21ef47eb904d265aeb768b5f3eaf8bd26a615acd
DarrenICR <darren.ichirante@gmail.com>
2019-06-03 19:50:49 +0200

    A jour 3

--- app/src/main/java/com/example/cinepop/controller/MainController.java
+++ app/src/main/java/com/example/cinepop/controller/MainController.java
@@ -1,5 +1,10 @@
 package com.example.cinepop.controller;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.widget.Toast;
+
 import com.example.cinepop.TMDbApi;
 import com.example.cinepop.model.Movie;
 import com.example.cinepop.model.MovieResponse;
@@ -11,32 +16,59 @@ import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
+
 public class MainController {
 
     public MainActivity view;
     private TMDbApi restApiMovie;
+
 
     public MainController(MainActivity view, TMDbApi restApiMovie) {
         this.view = view;
         this.restApiMovie = restApiMovie;
     }
 
+
+    public static boolean isConnectionAvailable(Context context) {
+
+        ConnectivityManager connectivityManager = (ConnectivityManager) context
+                .getSystemService(Context.CONNECTIVITY_SERVICE);
+        if (connectivityManager != null) {
+            NetworkInfo netInfo = connectivityManager.getActiveNetworkInfo();
+            if (netInfo != null && netInfo.isConnected()) {
+                return true;
+            }
+        }
+        return false;
+    }
+
     public void onCreate() {
 
-        Call<MovieResponse> call = restApiMovie.getMovies();
+        if (MainController.isConnectionAvailable(MainActivity.getAppContext())) {
 
-        call.enqueue(new Callback<MovieResponse>() {
-            @Override
-            public void onResponse(Call<MovieResponse> call, Response<MovieResponse> response) {
-                MovieResponse restMovieResponse = response.body();
-                List<Movie> movieList = restMovieResponse.getResults();
-                view.showList(movieList);
-            }
+            Call<MovieResponse> call = restApiMovie.getMovies();
 
-            @Override
-            public void onFailure(Call<MovieResponse> call, Throwable t) {
+            call.enqueue(new Callback<MovieResponse>() {
+                @Override
+                public void onResponse(Call<MovieResponse> call, Response<MovieResponse> response) {
+                    MovieResponse restMovieResponse = response.body();
+                    List<Movie> movieList = restMovieResponse.getResults();
+                    view.showList(movieList);
 
-            }
-        });
+                    view.SetCache(movieList);
+                }
+
+                @Override
+                public void onFailure(Call<MovieResponse> call, Throwable t) {
+                }
+            });
+        }
+        else {
+
+            List<Movie> movieList = view.GetCache();
+            view.showList(movieList);
+
+            Toast.makeText(MainActivity.getAppContext(), "Aucune connexion internet disponible", Toast.LENGTH_SHORT).show();
+        }
     }
 }
--- app/src/main/java/com/example/cinepop/view/MainActivity.java
+++ app/src/main/java/com/example/cinepop/view/MainActivity.java
@@ -1,28 +1,48 @@
 package com.example.cinepop.view;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.widget.Toast;
 
 import com.example.cinepop.Injection;
 import com.example.cinepop.R;
 import com.example.cinepop.controller.MainController;
 import com.example.cinepop.model.Movie;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class MainActivity extends Activity {
 
     private RecyclerView recyclerView;
     private RecyclerView.Adapter mAdapter;
     private RecyclerView.LayoutManager layoutManager;
-
     private MainController controller;
+    private static Context appContext;
+
+    private static final String KEY = "Movie_List";
+    SharedPreferences sharedPreferences;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        appContext = getApplicationContext();
+
         setContentView(R.layout.activity_main);
 
         recyclerView = findViewById(R.id.movies_list);
@@ -40,5 +60,40 @@ public class MainActivity extends Activity {
 
         mAdapter = new MyAdapter(movieList);
         recyclerView.setAdapter(mAdapter);
+    }
+
+    public static Context getAppContext() {
+        return appContext;
+    }
+
+    public void SetCache(List<Movie> movieList) {
+
+        Gson gson = new Gson();
+        String json = gson.toJson(movieList);
+
+        sharedPreferences = getPreferences(MODE_PRIVATE);
+
+        sharedPreferences
+                .edit()
+                .putString(KEY, json)
+                .apply();
+    }
+
+    public List<Movie> GetCache() {
+
+        sharedPreferences = getPreferences(MODE_PRIVATE);
+
+        if (sharedPreferences != null) {
+            Gson gson = new Gson();
+            List<Movie> movieList;
+
+            String string = sharedPreferences.getString(KEY, null);
+
+            Type type = new TypeToken<List<Movie>>() {
+            }.getType();
+            movieList = gson.fromJson(string, type);
+            return movieList;
+        }
+        return null;
     }
 }
